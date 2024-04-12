2c938bd22003e52ed49966145af8274f25d5db25
minhyeok0328 <alsgur7408@gmail.com>
2024-04-12 02:24:05 +0000

    날씨 api 가져오는 부분 작업

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,8 +1,20 @@
 # data_fetcher.py
 import requests
 
-class DataFetcher:
-    def __init__(self):
+PREFIX = 'https://api.openweathermap.org/data/2.5'
 
-    def fetch_weather(self):
-        return data
+class DataFetcher:
+    def __init__(self, api_key):
+        self.api_key = api_key
+
+    def fetch_weather(self, city):
+        try:
+            payload = {
+                'q': city,
+                'APPID': self.api_key
+            }
+            response = requests.get(f'{PREFIX}/weather', params=payload)
+
+            return response.text
+        except:
+            print('API를 호출하는 도중 문제가 발생하였습니다.')
