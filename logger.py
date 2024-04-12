8b6d816fba4ef0f1e9599473ac47dcd7d101a38e
Antonio.Choi <nmbertt1@naver.com>
2024-04-12 17:19:57 +0900

    logger 수정본 temp, name 변경

--- logger.py
+++ logger.py
@@ -5,19 +5,19 @@ class Logger:
     def logging(self, data):
         	## TO-DO ##
 		    # 전달받은 데이터를 파일로 저장한다.
-		self.data = processor.data
+		self.data = data
               ##?? need help  
               
-        with open("log.csv", "w", newline='') as file:
+        with open("log.csv", "a", newline='') as file: # w 를 a로 변경
             #로그 데이터를 쓰기 모드로 작성
             writer = csv.writer(file)
             # 열 작성
-            writer.writerow(["coord", "weather", "base", "main", "visibility", "wind", "clouds", "dt", "sys", "timezone", "id","name", "cod"])
+            writer.writerow(["main", "name"])
             # 열 추가
 			for row in file:
                 writer.writerow(row)
 		
-		return
+	    return
 
             
         
