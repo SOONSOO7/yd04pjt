39718fc180d4c44e4a92bf87aeff04643f5fcd31
minhyeok0328 <alsgur7408@gmail.com>
2024-04-12 09:28:45 +0000

    log 데이터 불러와서 온도 데이터 계속 적재되도록 변경

--- main.py
+++ main.py
@@ -2,20 +2,21 @@
 from data_fetcher import DataFetcher
 from data_processor import DataProcessor
 from visualizer import Visualizer
-# from logger import Logger
+from logger import Logger
 
 def main():
     api_key = '575688733bd584393af21c742c7acd64'  # openweathermap API 키 입력
     city = input("Which City? ")
+    LOG_FILE_NAME = 'log.csv'
 
     fetcher = DataFetcher(api_key)
     processor = DataProcessor()
-    visualizer = Visualizer()
-    # logger = Logger()
+    visualizer = Visualizer(LOG_FILE_NAME)
+    logger = Logger(LOG_FILE_NAME)
     
     raw_data = fetcher.fetch_weather(city)
     processed_data = processor.process_data(raw_data)
-    # logger.save_data(processed_data)
+    logger.logging(processed_data)
     visualizer.plot_data(processed_data)
 
 if __name__ == "__main__":
--- visualizer.py
+++ visualizer.py
@@ -1,13 +1,27 @@
 # visualizer.py
 import plotly.graph_objects as go
+import csv
+from os.path import exists
 
 class Visualizer:
+    def __init__(self, file_name):
+        self.file_name = file_name
+
     def plot_data(self, data):
+        temperatures = []
+        city = data['name']
+
         if not data['main']:
             return
 
+        if exists(self.file_name):
+            with open(self.file_name, 'r', newline='') as file:
+                reader = csv.reader(file)
+                for row in reader:
+                    if row[0] == city:
+                        temperatures = [x for idx, x in enumerate(row) if idx != 0]
+
         #temperatures = [entry['temperature'] for entry in data]
-        temperatures = [] # samples
         temperatures.append(data['main']['temp'])
         fig = go.Figure()
         fig.add_trace(go.Scatter(y=temperatures, mode='lines+markers', name='Temperature'))
